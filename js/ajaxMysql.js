385703e61b91337b8143f4acbb23a27af8052d97
Tommaso Levato <tommaso.levato@gmail.com>
2013-09-03 00:07:01 +0200

    refactor

--- js/ajaxMysql.js
+++ js/ajaxMysql.js
@@ -1,108 +1,12 @@
-function getPanorama(Id)
-{
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-//xmlhttp.onreadystatechange=function()
-//  {
-//  if (xmlhttp.readyState===4 && xmlhttp.status===200)
-//    {
-//    
-//    }
-//  }
-xmlhttp.open("GET","getPanorama.php?q="+Id,false);
-xmlhttp.send();
-}
-
-
-
-
-
-function getNextPanorama(Id)
-{
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-//xmlhttp.onreadystatechange=function()
-//  {
-//  if (xmlhttp.readyState===4 && xmlhttp.status===200)
-//    {
-//    
-//    }
-//  }
-xmlhttp.open("GET","getNextPanorama.php?q="+Id,false);
-xmlhttp.send();
-}
-
-function getObject(Id)
-{
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-//xmlhttp.onreadystatechange=function()
-//  {
-//  if (xmlhttp.readyState===4 && xmlhttp.status===200)
-//    {
-//    
-//    }
-//  }
-xmlhttp.open("GET","getObject.php?q="+Id,false);
-xmlhttp.send();
-}
-
-function getText(ObjectName)
-{
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-//xmlhttp.onreadystatechange=function()
-//  {
-//  if (xmlhttp.readyState===4 && xmlhttp.status===200)
-//    {
-//    
-//    }
-//  }
-xmlhttp.open("GET","getText.php?q="+ObjectName,false);
-xmlhttp.send();
-}
-
-function getImage(ObjectName)
-{
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-//xmlhttp.onreadystatechange=function()
-//  {
-//  if (xmlhttp.readyState===4 && xmlhttp.status===200)
-//    {
-//    
-//    }
-//  }
-xmlhttp.open("GET","getImage.php?q="+ObjectName,false);
-xmlhttp.send();
+function getContent(param, phpFile) {
+    if (window.XMLHttpRequest)
+    {//IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else
+    {//IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET", phpFile + ".php?q=" + param, false);
+    xmlhttp.send();
 }
