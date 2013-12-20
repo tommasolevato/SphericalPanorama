a44c4c783b86892226b205ab2b53f32662b0a584
Tommaso Levato <tommaso.levato@gmail.com>
2013-12-20 14:34:23 +0100

    Update eventListeners.js

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -2,7 +2,6 @@ var isUserInteracting = false;
 var isRightClick = false;
 var noZoomLevel = 70;
 
-
 function onDocumentMouseDown(event) {
     event.preventDefault();
     if (event.which === 3) {
@@ -18,55 +17,33 @@ function onDocumentMouseDown(event) {
     var vector = new THREE.Vector3(mouseX, mouseY, 0.5);
     projector.unprojectVector(vector, camera);
     var raycaster = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
-    var intersects = raycaster.intersectObjects(objects, true);
+    var intersects = raycaster.intersectObjects(markers, true);
     if (intersects[0] !== undefined) {
-        interactiveObject = intersects[0].object.parent;
+
+        interactiveObject = intersects[0].object;
         onMouseDownObjectXRotation = interactiveObject.rotation.x;
         onMouseDownObjectYRotation = interactiveObject.rotation.y;
         onMouseDownObjectZRotation = interactiveObject.rotation.z;
-        if (interactiveObject.name === "marker.obj") {
-            getMenu();
-        }
+        manageHotspot();
+        //portal(..);
+        //if (interactiveObject.geometry.name) {
+        //  onHotspotClicked();
+        //}
     }
-    var intersects2 = raycaster.intersectObjects(interactiveTexts, true);
-    if (intersects2.length === 0)
-        intersects2 = raycaster.intersectObjects(menuPoints, true);
-    if (intersects2[0] !== undefined) {
-        var information = intersects2[0].object.name.split(" ");
+//    var intersects2 = raycaster.intersectObjects(interactiveTexts, true);
+//    if (intersects2.length === 0)
+//        intersects2 = raycaster.intersectObjects(menuPoints, true);
+//    if (intersects2[0] !== undefined) {
+//        var information = intersects2[0].object.name.split(" ");
 //        var hotspotArray = getContent("hotspotInfo", information[0]);
 //        var hotspotPosition = intersects2[0].object.position;
-//        var hotspot = hotspotArray.pop();
-//        var hotspotInfo = hotspot[information[1]];
-//        var heigth;
-//        var width;
-//        switch (information[1]) {
-//            case "PDF":
-//                heigth = hotspot.PDF_HEIGTH;
-//                width = hotspot.PDF_HEIGTH;
-//                break;
-//            case "Video":
-//                heigth = hotspot.VIDEO_HEIGTH;
-//                width = hotspot.VIDEO_HEIGTH;
-//                break;
-//            case "Gallery":
-//                heigth = hotspot.GALLERY_HEIGTH;
-//                width = hotspot.GALLERY_HEIGTH;
-//                break;
-//            case "Object":
-//                heigth = hotspot.OBJECT_HEIGTH;
-//                width = hotspot.OBJECT_HEIGTH;
-//                break;
-
-
-        var hotspotArray = getContent("hotspotInfo", information[0]);
-        var hotspotPosition = intersects2[0].object.position;
-        while (hotspotArray.length > 0) {
-            var hotspot = hotspotArray.pop();
-            if (hotspot['Name'] === information[1]) {
-                portal(hotspot['Source'], hotspotPosition, hotspot['Width'], hotspot['Height']);
-            }
-        }
-    }
+//        while (hotspotArray.length > 0) {
+//            var hotspot = hotspotArray.pop();
+//            if (hotspot['Name'] === information[1]) {
+//                portal(hotspot['Source'], hotspotPosition, hotspot['Width'], hotspot['Height']);
+//            }
+//        }
+//    }
 }
 
 
@@ -84,15 +61,16 @@ function onDocumentMouseMove(event) {
         lat = Math.max(rawLat, latLimit);
     }
     if (interactiveObject !== undefined && !isRightClick) {
-        interactiveObject.rotation.y = (event.clientX - onPointerDownPointerX) * 0.01 + onMouseDownObjectYRotation;
-        interactiveObject.rotation.z = (onPointerDownPointerY - event.clientY) * 0.01 + onMouseDownObjectZRotation;
+//        interactiveObject.rotation.y = (event.clientX - onPointerDownPointerX) * 0.01 + onMouseDownObjectYRotation;
+//        interactiveObject.rotation.z = (onPointerDownPointerY - event.clientY) * 0.01 + onMouseDownObjectZRotation;
     }
     if (interactiveObject !== undefined && isRightClick) {
-        interactiveObject.rotation.x = (onPointerDownPointerY - event.clientY) * 0.01 + onMouseDownObjectXRotation;
+//        interactiveObject.rotation.x = (onPointerDownPointerY - event.clientY) * 0.01 + onMouseDownObjectXRotation;
     }
     sprite.position.set(event.clientX, event.clientY - 20, 0);
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    //printLonLatInfo();
 }
 
 function onDocumentMouseUp(event) {
@@ -104,20 +82,28 @@ function onDocumentMouseUp(event) {
 
 function onDocumentMouseWheel(event) {
     if (!amILoading) {
-        var previousFov = fov;
-        var sub = fov - Math.min((event.wheelDeltaY * 0.05), 2);
+        //var previousFov = fov;
+        var delta;
+        if (event.wheelDeltaY) {
+            delta=event.wheelDeltaY;
+        } else if (event.wheelDelta) {
+            delta=event.wheelDelta;
+        } else if (event.detail) {
+            delta=event.detail;
+        }
+        var sub = fov - Math.min((delta * 0.05), 2);
         var zoom = maxZoom;
         var indice = whichTransitionDirection();
-        if (indice !== undefined) {
+        if (indice !== undefined & delta>0) {
             zoom = ZoomArray[indice]['ZoomNext'];
         }
         if (zoom <= sub && sub <= minZoom) {
             if (event.wheelDeltaY) {
-                fov -= Math.min((event.wheelDeltaY * 0.05), 2);
+                fov -= Math.min((delta * 0.05), 2);
             } else if (event.wheelDelta) {
-                fov += Math.min((event.wheelDelta * 0.05), 2);
+                fov += Math.min((delta * 0.05), 2);
             } else if (event.detail) {
-                fov += Math.min((event.detail * 1.0), 2);
+                fov += Math.min((delta * 1.0), 2);
             }
 
             if (planeMesh !== undefined) {
@@ -127,9 +113,23 @@ function onDocumentMouseWheel(event) {
             camera.projectionMatrix.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
             render();
         }
-        if (sub < zoom) {
-            getNewPanorama();
+        if (sub < zoom & delta>0) {
+            console.log(panoId);
+            getNewPanorama(panoId);
         }
+        //Zoom Previous
+        if(delta<0 & fov>69.){ // 69. FIXME
+            var previousPanoArray=getContent("previousPano", panoId);
+            while(previousPanoArray.length>0){
+                var candidatePreviousPano=previousPanoArray.pop();
+                if(Math.abs(lat-candidatePreviousPano['LatitudeOnLoad'])<20 &
+                        Math.abs(lon-candidatePreviousPano['LongitudeOnLoad'])<20){
+                    load(candidatePreviousPano['IdCalling'],candidatePreviousPano['Latitude'],candidatePreviousPano['Longitude'] );
+                    //qui non passo i valori attuali perché non voglio fare la transizione smooth
+                }
+            }
+        }
+        
     }
 }
 
@@ -147,7 +147,7 @@ function onDocumentDoubleclick(event) {
         render();
     }
     else {
-        var found = getNewPanorama();
+        var found = getNewPanorama(panoId);
         if (!found) {
             fov = 70;
             camera.projectionMatrix.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
@@ -162,6 +162,7 @@ function onDocumentDoubleclick(event) {
 
 function onDocumentRightClick(event) {
     isRightClick = true;
+    onHotspotClick();
     event.preventDefault();
 }
 
@@ -177,14 +178,16 @@ function isZoomIn(previousFov, fov) {
 
 //FIXME: non è proprio precisissima a quanto pare
 function XYZtoLonLat(x, y, z) {
-
     var lonLat = [];
     lonLat[1] = Math.acos(y / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2))) - Math.PI / 2;
     lonLat[1] *= 180 / Math.PI;
-    lonLat[1] = Math.max(lonLat[1], latLimit);//BOH
-    lonLat[0] = Math.atan(z / x);
+    lonLat[1] = Math.max(lonLat[1], latLimit); //BOH
+    if (x >= 0)
+        lonLat[0] = Math.atan(z / x);
+    else
+        lonLat[0] = Math.atan(z / x) + Math.PI;
     lonLat[0] *= 180 / Math.PI;
-    lonLat[0] = mod(lonLat[0], 360);//BOH
+    lonLat[0] = mod(lonLat[0], 360); //BOH
     return lonLat;
-
+    
 }
